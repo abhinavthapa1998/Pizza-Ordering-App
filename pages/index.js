20be7ead4f99278ffdf109aad17fdf98bb4cb37a
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 17:22:44 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Pizza Restaurant in New York</title>
+        <meta name="description" content="Best Pizza Shop in town!" />
+      </Head>
+      Homepage
+      <img src="https://img.freepik.com/free-vector/flying-slice-pizza-cartoon-vector-illustration-fast-food-concept-isolated-vector-flat-cartoon-style_138676-1934.jpg?w=2000" />
+    </div>
+  );
+}
