30f53a6767ef4a87c221dc04a147a250d2ca5a1b
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 17:59:24 +0530

    Featured Component Start

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,8 +18,32 @@ const Navbar = () => {
           <div className={styles.text}>012-345-678</div>
         </div>
       </div>
-      <div className={styles.item}>Center</div>
-      <div className={styles.item}>Right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/685/685352.png"
+            alt="logo"
+            width={160}
+            height={69}
+          />
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"
+            alt="logo"
+            width={30}
+            height={30}
+          />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
