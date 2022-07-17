9f1d29c0c6e9cf82530b2daa31ce0077dc2452a4
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 18:09:03 +0530

    Pre-Pizza Card/List

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,16 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
-// import Image from "next/image";
+
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <img
-            src="https://www.pngitem.com/pimgs/b/177-1777154_phone-symbol-png.png"
-            alt="telephone"
-            height={32}
-            width={32}
-            layout="fill"
-          />
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW!</div>
-          <div className={styles.text}>012-345-678</div>
+          <div className={styles.text}>012 345 678</div>
         </div>
       </div>
       <div className={styles.item}>
@@ -23,24 +18,15 @@ const Navbar = () => {
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/685/685352.png"
-            alt="logo"
-            width={160}
-            height={69}
-          />
+          <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+          <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
       <div className={styles.item}>
         <div className={styles.cart}>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"
-            alt="logo"
-            width={30}
-            height={30}
-          />
+          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
           <div className={styles.counter}>2</div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
+import styles from "../styles/Home.module.css";
 import Head from "next/head";
+import Featured from "../components/Featured";
 
 export default function IndexPage() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>Pizza Restaurant in New York</title>
         <meta name="description" content="Best Pizza Shop in town!" />
       </Head>
-      Homepage
-      <img src="https://img.freepik.com/free-vector/flying-slice-pizza-cartoon-vector-illustration-fast-food-concept-isolated-vector-flat-cartoon-style_138676-1934.jpg?w=2000" />
+      <Featured />
     </div>
   );
 }
